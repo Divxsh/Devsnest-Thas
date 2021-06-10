97486581338671bd95dbe226091ca535fc4119d0
Divash Gupta <guptadivash70@gmail.com>
2021-06-10 22:15:33 +0530

    Simple Calculator

--- Day 5/Index.js
+++ Day 5/Index.js
@@ -2,23 +2,23 @@ function sum() {
     var n1 = parseInt(document.getElementById("num1").value);
     var n2 = parseInt(document.getElementById("num2").value);
     x = n1 + n2;
-    document.getElementById("output").innerHTML = ("Output : <br>" + x);
+    document.getElementById("output").innerHTML = x;
 }
 function sub() {
     var n1 = parseInt(document.getElementById("num1").value);
     var n2 = parseInt(document.getElementById("num2").value);
     x = n1 - n2;
-    document.getElementById("output").innerHTML = ("Output : <br>" + x);
+    document.getElementById("output").innerHTML =x;
 }
 function mul() {
     var n1 = parseInt(document.getElementById("num1").value);
     var n2 = parseInt(document.getElementById("num2").value);
     x = n1 * n2;
-    document.getElementById("output").innerHTML = ("Output : <br>" + x);
+    document.getElementById("output").innerHTML = x;
 }
 function divide() {
     var n1 = parseFloat(document.getElementById("num1").value);
     var n2 = parseFloat(document.getElementById("num2").value);
     x = n1 / n2;
-    document.getElementById("output").innerHTML = ("Output : <br>" + x);
+    document.getElementById("output").innerHTML = x;
 }
